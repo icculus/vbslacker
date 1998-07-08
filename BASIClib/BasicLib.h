1d6652fa3997146054bc15a03ed696fbb5145f88
Ryan C. Gordon <icculus@icculus.org>
1998-07-08 10:27:26 +0000

    initial release.

---
+++ BASIClib/BasicLib.h
@@ -0,0 +1,25 @@
+/*
+ * Universal header file; include this to include all the
+ *  BASIClib headers.
+ *
+ *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_BASICLIB_H_
+#define _INCLUDE_BASICLIB_H_
+
+#include "InternalMemManager.h"
+#include "BasicString.h"
+#include "Variant.h"
+#include "Boolean.h"
+#include "OnEvents.h"
+#include "ErrorFunctions.h"
+#include "MathFunctions.h"
+#include "ConversionFunctions.h"
+#include "TimeDateFunctions.h"
+
+#endif
+
+/* end of BasicLib.h ... */
+
+
