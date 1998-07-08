df236a0dc2f7796d08bf06645d3873cec9f912fc
Ryan C. Gordon <icculus@icculus.org>
1998-07-08 12:37:21 +0000

    initial release.

---
+++ BASIClib/Initialize.c
@@ -0,0 +1,21 @@
+/*
+ * Initialization function. Call __initBasicLib() before doing anything
+ *  else with the library. This function just calls each other sections'
+ *  initialization function, if one exists.
+ *
+ *     params : void.
+ *    returns : void.
+ */
+
+#include "Initialize.h"
+#include "OnEvents.h"
+#include "TimeDateFunctions.h"
+
+void __initBasicLib(void)
+{
+    __initOnEvents();
+    __initTimeDateFunctions();
+} /* __initBasicLib */
+
+/* end of Initialize.c ... */
+
