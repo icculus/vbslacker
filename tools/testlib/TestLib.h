bcab75db1f917b32417184721cbf1f264a10e329
Ryan C. Gordon <icculus@icculus.org>
1998-07-11 10:27:38 +0000

    initial release. Add stuff for the mainline to call in here.

---
+++ tools/testlib/TestLib.h
@@ -0,0 +1,15 @@
+/*
+ * Prototypes from various BASIClib testing subsystems.
+ *
+ *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_TESTLIB_H_
+#define _INCLUDE_TESTLIB_H_
+
+void testConversions(void);
+
+#endif
+
+/* end of testlib.h ... */
+
