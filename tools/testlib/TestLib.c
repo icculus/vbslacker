5a333d7bae14a279c2c847797fdb18eff1bea387
Ryan C. Gordon <icculus@icculus.org>
1998-07-11 10:28:45 +0000

    initial release; mainline for testlib program.

---
+++ tools/testlib/TestLib.c
@@ -0,0 +1,15 @@
+/*
+ * Mainline for BASIClib testing routines.
+ *
+ *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#include "TestLib.h"
+
+void main(void)
+{
+    testConversions();
+} /* main */
+
+/* end of TestLib.c ... */
+
