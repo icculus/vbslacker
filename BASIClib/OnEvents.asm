336c9af7a8719881775db10b446bc90f8c88d451
Ryan C. Gordon <icculus@icculus.org>
1998-07-12 16:56:22 +0000

    WAYS to go, will probably crash your system if run, but at least it assembles.  :)

--- BASIClib/OnEvents.asm
+++ BASIClib/OnEvents.asm
@@ -6,53 +6,77 @@
 global __callOnEventHandler
 
 ; This procedure copies the stack from event handling procedure to top
-;  of stack, calls event handler, and copies stack back. Neet, huh?
+;  of stack, calls event handler, and deals with returns. Neet, huh?
+;
+; The stack is copied to fool the called address into thinking it is
+;  executing as normal, with access to all variables, both arguments and
+;  local, as they were originally. Before calling the handler, we patch
+;  the copied stack so the return address will be in this procedure. This
+;  allows us to clean up and return the stack to its normal state. Also,
+;  This allows us to "skip" any functions called between the handler's
+;  procedure and where the event was triggered.
 ;
 ;     params : POnEventHandler of handler to call at [esp + 4].
+;              (This will be [ebp + 8] when we set up the base ptr.)
 ;    returns : any return value from BASIC routine in EAX.
 
-__callOnEventHandler    proc
+__callOnEventHandler:                   ; proc near
         push    ebp                     ; Save calling function's base ptr.
         mov     ebp,esp                 ; Move stack ptr to base ptr.
 
-        push    ebx                     ; Save registers...
+        push    esi                     ; Save registers...
+        push    edi
+        push    ebx
 
         mov     ebx,[ebp + 8]           ; Store POnEventHandler in EBX.
-        mov     eax,[ebx]               ; Store handlerAddr in EAX.
+        mov     edi,[ebx]               ; Store handlerAddr in EDI.
         mov     edx,[ebx + 8]           ; Store stackEnd in EDX.
+        mov     esi,[ebx + 12]          ; Store origReturnAddr in ESI.
         mov     ebx,[ebx + 4]           ; Store stackStart in EBX.
-        mov     ecx,ebx                 ;  ..save extra copy in ECX.
 
+        mov     dword [esi],@returnloc        ; patch return address on stack.
+
+        mov     esi,ebx                 ; Put stack start in ESI.
+        mov     ebx,esp                 ; Copy stack pointer to EBX.
+
+        std                             ; decrement on LODSB...
 @stackcopy:                             ; loop to move stack...
-        cmp     edx,ebx                 ; have we hit the end?
+        cmp     edx,esi                 ; have we hit the end?
         ja      @endstackcopy           ; then stop.
-        mov     byte ptr [esp],[ebx]    ; copy a byte.
+        lodsb                           ; Put byte in AL, decrement ESI.
+        mov     [ebx],cl    ; copy a byte.
         dec     esp                     ; point to next byte...
         dec     ebx                     ;   ...remember stack goes downward.
         jmp     @stackcopy              ; do it again.
 @endstackcopy:
 
+;
+; EAX handlerAddr ... overwritten by callee with retVal.
+; EBX copy of base ptr ... preserved by callee.
+; ECX not used
+; EDX
+; ESI
+; EDI
+; ESP
+; EBP
+;
+
         mov     ebx,ebp                 ; Save copy of base pointer.
         mov     ebp,esp                 ; Fake the base ptr for handler.
-        call    [eax]                   ; execute BASIC error handler.
+        jmp     [edi]                   ; execute BASIC error handler.
                                         ;  retVal is in EAX.
+@returnloc:
+
         mov     ebp,ebx                 ; Move base pointer back.
 
-@movestackback:                         ; Move it back.
-        cmp     edx,ecx                 ; At start of stack (ECX)?
-        jb      @endstackback           ;  then stop.
-        mov     byte ptr [edx],[esp]    ; copy a byte.
-        inc     edx                     ;  (inc?) decrement pointers.
-        inc     esp
-        jmp     @movestackback          ; do it again.
-@endstackback:
-
         pop     ebx                     ; Restore registers...
+        pop     edi
+        pop     esi
 
         mov     esp,ebp                 ; Restore original stack pointer.
         pop     ebp                     ; Restore calling function's base ptr.
         ret                             ; Return.
-__callOnEventHandler    endp
+;__callOnEventHandler    endp
 
 ; end of OnEvents.asm ...
 
