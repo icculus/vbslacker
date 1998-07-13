f9580a8efb6ba9ee12c7f17dabceea7724036b59
Ryan C. Gordon <icculus@icculus.org>
1998-07-13 10:22:33 +0000

    Added extern _stack_ptr_ and _base_ptr_, __getBasePointer() macro, updated __registerOnEventHandler() and OnEventHandler prototypes to take basePtr. etc.

--- BASIClib/OnEvents.h
+++ BASIClib/OnEvents.h
@@ -29,24 +29,49 @@ typedef struct
     void *stackStart;
     void *stackEnd;
     void *origReturnAddr;
+    void *basePtr;
 } OnEventHandler;
 
 typedef OnEventHandler *POnEventHandler;
 
+extern void* _stack_ptr_;
+extern void* _base_ptr_;
 
-/* !!! ...hope I'm doing this right...
- * God, inline assembly is SCARY lookin' in gcc, isn't it?
- *
+/* !!! ...hope I'm doing this right... */
+
+/* God, inline assembly is SCARY lookin' in gcc, isn't it? */
+
+/*
  *  Get the stack pointer, and store it in *retVal.
  */
 #define __getStackPointer(retVal) __asm__ __volatile__ ("movl %%esp, %0\n\t" \
                                                         : "=q" (*retVal) )
 
+/*
+ *  Get the base pointer, and store it in *retVal.
+ */
+#define __getBasePointer(retVal) __asm__ __volatile__ ("movl %%ebp, %0\n\t" \
+                                                        : "=q" (*retVal) )
+
+/*
+ * Insert this right AFTER a line label that serves as the start of an
+ *  ON EVENT handler.
+ *
+ * The i386 version just guarantees that optimizations aren't saving any
+ *  important data in the registers. By telling gcc we fucked with memory, it
+ *  believes we're "clobbered" all the registers, and will reload anything
+ *  stored in them.
+ */
+#define __markOnEventHandlerAddr __asm__ __volatile__ ("nop\n\t" : \
+                                                        : /* no output */ \
+                                                        : /* no input */ \
+                                                          "memory" );
+
 POnEventHandler __getOnEventHandler(OnEventTypeEnum evType);
 void __initOnEvents();
 void __registerOnEventHandler(void *handlerAddr, void *stackSize,
                               void *stackEnd, void *origReturnAddr,
-                              OnEventTypeEnum evType);
+                              void *basePtr, OnEventTypeEnum evType);
 
 void __deregisterOnEventHandler(void *handlerAddr, OnEventTypeEnum evType);
 
