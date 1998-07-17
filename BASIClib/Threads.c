53bcc7871e646fecdf3256b97a367eec0990c872
Ryan C. Gordon <icculus@icculus.org>
1998-07-17 22:27:55 +0000

    added __(de)initThread() calls, and other updates.

--- BASIClib/Threads.c
+++ BASIClib/Threads.c
@@ -4,14 +4,37 @@
  *  Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
  */
 
+/* !!! comment this thing. */
+
+#include <stdlib.h>
 #include "Threads.h"
 
-/* !!! comment this thing. */
+static volatile int criticalSectionCount = 0;
+static volatile int *indexes;
+static volatile int threadCount = 0;
+
+    /* defined in Initialize.c, but only declared here for abstractness. */
+void __initThread(int tidx); 
+void __deinitThread(int tidx); 
+
+void __initThreads(void)
+{
+    threadCount++;
+    indexes = malloc(sizeof (int));
+    /* !!! indexes[0] = __OS__getTID(); */
+
+    __newThreadInit(0);
+} /* __initThreads */
 
 int __getThreadCount(void)
 {
-    return(1);
+    return(threadCount);
 } /* __getThreadCount */
+
+int __getHighestThreadIndex(void)
+{
+    return(0);
+} /* __getHighestThreadIndex */
 
 int __getCurrentThreadIndex(void)
 {
@@ -22,12 +45,18 @@ void __threadTimeslice(void)
 {
 } /* __threadTimeslice */
 
+
 void __enterCriticalThreadSection(void)
 {
+    criticalSectionCount++;
 } /* __enterCriticalThreadSection */
+
 
 void __exitCriticalThreadSection(void)
 {
+    criticalSectionCount--;
+    if (criticalSectionCount == 0)
+        ; /* do something. */
 } /* __exitCriticalThreadSection */
 
 
