43fc7e39325bcfdc3688dccb890a0e3e4a53cb7a
Ryan C. Gordon <icculus@icculus.org>
1998-07-20 07:19:17 +0000

    added ...er, something.

--- tools/testlib/TestLib.c
+++ tools/testlib/TestLib.c
@@ -4,11 +4,21 @@
  *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
  */
 
+#include <stdio.h>
+#include "Initialize.h"
 #include "TestLib.h"
 
 void main(void)
 {
+    setbuf(stdout, NULL);
+    printf("Making call to __initBasicLib()...\n"); 
+    __initBasicLib();
+    testOnEvents();
     testConversions();
+
+        /* !!! This will be done automatically, eventually... */
+    printf("Making call to __deinitBasicLib()...\n"); 
+    __deinitBasicLib();
 } /* main */
 
 /* end of TestLib.c ... */
