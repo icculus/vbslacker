4842e658605b45445e012a4f35fad083718ea90b
Ryan C. Gordon <icculus@icculus.org>
1998-07-20 21:31:08 +0000

    Major updates. Lots more debugging help.

--- tools/testlib/TestOnEvents.c
+++ tools/testlib/TestOnEvents.c
@@ -6,72 +6,130 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "TestLib.h"
 #include "OnEvents.h"
 #include "ErrorFunctions.h"
-
+#include "Boolean.h"
 
 /* !!! Comment functions. */
 
 #define TESTVAR_VALUE1 0xABCD
 #define TESTVAR_VALUE2 0x1234
 
-#define STR1 "The Quick brown fox"
-#define STR2 "jumped over the lazy dog."
-void test_getBasePointer(void)
+#define RECURSION_COUNT 3
+
+#define FAILED "  - Failed.\n"
+#define FAILED_WITH_WARNING "  - Failed. Later tests will fail, too.\n"
+
+int recursive = 0;
+
+boolean binaryDump(char *fileName, void *data, int size)
 /*
- * This function is very i386 specific. Your mileage may vary.
+ * Dump a chunk of memory to a binary file.
+ *
+ *   params : fileName == file to try to write to. It will destroy
+ *                         any previous contents.
+ *            data     == memory to write.
+ *            size     == number of bytes to write, starting with (data).
+ *  returns : boolean true if data written, boolean false otherwise.
+ */
+{
+    FILE *binStream = fopen(fileName, "wb");
+    boolean retVal = false;
+
+    if (binStream == NULL)
+        printf("  - COULDN'T OPEN [%s] FOR BINARY OUTPUT!\n", fileName);
+    else
+    {
+        if (fwrite(data, size, 1, binStream) != 1)
+            printf("  - COULDN'T WRITE DATA DUMP TO DISK!\n");
+        else
+        {
+            printf("  - Data dump is in [%s]...\n", fileName);
+            retVal = true;
+        } /* else */
+        fclose(binStream);
+    } /* else */
+
+    return(retVal);
+} /* binaryDump */
+
+
+void test__getBasePointer(void)
+/*
+ * This function is pretty i386 specific. Your mileage may vary.
  *
  *  !!! more comments.
  */
 {
-    char var1[] = STR1;
-    char var2[] = STR2;
-    
-    char *loc1;
-    char *loc2;
+    int var1 = TESTVAR_VALUE1;
 
-#ifdef DEBUG
-    FILE *debugStream;
-    char *i;
-#endif
-
-    printf("Testing __getBasePointer()...");
+    printf("Testing __getBasePointer()...\n");
 
     __getBasePointer(&_base_ptr_);
-    loc1 = (char *) (((int) _base_ptr_) + 8 + strlen(STR1));
-    loc2 = (char *) (((int) loc1) + strlen(STR2));
 
-    if ( (strcmp(loc1, STR1) != 0) || (strcmp(loc2, STR2) != 0) )
+    if (*((int *) (((int) _base_ptr_) - 4)) != TESTVAR_VALUE1)
     {
-        printf("failed.");
-
-#ifdef DEBUG
-        printf("var1 == [%s]...should be [%s]\n", loc1, var1);
-        printf("var2 == [%s]...should be [%s]\n", loc2, var2);
-
-        debugStream = fopen("./debugBP.bin", "wb");
-        if (debugStream == NULL)
-            printf("Couldn't open ./debugBP.bin for output!\n");
-        else
-        {
-            printf("Check ./debugBP.bin for stack dump...\n");
-            printf("It contains bytes from bp-20 to bp+20...\n");
-            for (i = _base_ptr_ - 20; i <= _base_ptr_ + 20; i++)
-                fputc(debugStream, *i);
-            fclose(debugStream);
-        } /* else */
-#endif
-
+        printf(FAILED_WITH_WARNING);
+        printf("  - dword [ebp] should have been (0x%X), but is (0x%X).\n",
+                TESTVAR_VALUE1, *((int *) (((int) _base_ptr_) - 4)));
+        if (binaryDump("debugBP.bin", _base_ptr_ - 100, 200) == true)
+            printf("  - (That's our supposed [ebp-100] to [ebp+100]...)\n");
     } /* if */
+
+    var1 = 0;  /* prevent compiler warnings... */
+} /* test__getBasePointer */
+
+
+void test__getStackPointer_recurse(void)
+/*
+ * This function calls itself RECURSION_COUNT times, to pile
+ *  some data on the stack.
+ */
+{
+    recursive++;
+    if (recursive == RECURSION_COUNT)
+        __getStackPointer(&_stack_ptr_);
     else
-        printf("successful.");
-} /* test_getBasePointer */
+        test__getStackPointer_recurse();
+} /* test__getStackPointer_recurse */
 
 
-void testOnErrorHandling(int x)
+void test__getStackPointer()
+/*
+ * The theory being that a void function with void arguments should
+ *  add 8 bytes to the stack; four for the return address and four for the
+ *  saved base pointer. So we grab the current stack pointer, and call
+ *  a recursive function that'll grab the stack pointer before returning.
+ *  By doing the math between the two pointers, we can see if we're
+ *  grabbing it correctly.
+ */
+{
+    void *expected;
+    void *sp;
+
+    printf("Testing __getStackPointer()...\n");
+
+    __getStackPointer(&sp);
+
+    expected = (sp - (RECURSION_COUNT * 8));
+
+    recursive = 0;
+    test__getStackPointer_recurse();
+
+    if (_stack_ptr_ != expected)
+    {
+        printf(FAILED_WITH_WARNING);
+        printf("  - expected (%p), but got (%p)\n", expected, _stack_ptr_);
+    } /* if */
+} /* test__getStackPointer */
+
+
+void testOnErrorGotoHandling(int x)
 {
     int testVar1 = TESTVAR_VALUE1;
     int testVar2 = TESTVAR_VALUE2;
+    char testVar3[] = "Look for this string in the data dump!";
     int landed = 0;
 
     printf("Testing ON ERROR GOTO handling...");
@@ -89,15 +147,17 @@ errHandler:
     __markOnEventHandlerAddr;
     if ((testVar1 != TESTVAR_VALUE1) || (testVar2 != TESTVAR_VALUE2))
     {
-        __getBasePointer(&_base_ptr_);   /* !!! lose this! */
-        printf("Base pointer inside error handler is (%p)\n", _base_ptr_);
+        __getBasePointer(&_base_ptr_); 
 
-        printf("\n  Base pointer is damaged.\n");
-        printf("  testVar1 is 0x%X, should be 0x%X...\n",
+        printf("  - Base pointer is damaged.\n");
+        printf("  - EPB inside error handler is (%p)\n", _base_ptr_);
+        printf("  - testVar1 is (0x%X), should be (0x%X)...\n",
                                  testVar1, TESTVAR_VALUE1);
-        printf("  testVar2 is 0x%X, should be 0x%X...\n",
+        printf("  - testVar2 is (0x%X), should be (0x%X)...\n",
                                  testVar2, TESTVAR_VALUE2);
-        printf("  ...Stopping tests.\n");
+        if (binaryDump("debugErrGoto.bin", _base_ptr_ - 100, 200) == true)
+            printf("  - (That's our supposed [ebp-100] to [ebp+100]...)\n");
+        printf("  - Stack will be FUBAR. Must terminate testing.\n");
         exit(1);
     } /* if */
 
@@ -111,19 +171,25 @@ missedHandler:
 endTest:
     switch (landed)
     {
-        case 1:
-            printf("Successful.\n");
+        case 1:   /* good. */
             break;
         case 2:
-            printf("Handler missed. Failed.\n");
+            printf("  - Handler missed. %s", FAILED);
             break;
-        case 0:
         default:
-            printf("Confused! Failed.\n");
+            printf("  - Confused! %s", FAILED);
             break;
     } /* switch */
 
+    testVar3[0] = '\0';  /* stops compiler whining. */
+
     __deregisterOnEventHandler(&x + sizeof (x), ONERROR);
+} /* testOnErrorGotoHandling */
+
+
+void testOnErrorHandling(void)
+{
+    testOnErrorGotoHandling(0);
 } /* testOnErrorHandling */
 
 
@@ -136,10 +202,9 @@ void testOnEvents(void)
  */
 {
     printf("Testing ON EVENT handling...\n");
-    printf("  (Note these might crash the program really horribly...)\n\n");
-
-    test_getBasePointer();
-    testOnErrorHandling(0);
+    test__getBasePointer();
+    test__getStackPointer();
+    testOnErrorHandling();
 } /* testOnEvents */
 
 
