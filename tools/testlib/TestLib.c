b7e3890c05a0ffcac39eafb2fca71b830d97eb0b
Ryan C. Gordon <icculus@icculus.org>
1998-07-23 04:45:20 +0000

    Prettied up output, other minor stuff.

--- tools/testlib/TestLib.c
+++ tools/testlib/TestLib.c
@@ -11,12 +11,15 @@
 int main(void)
 {
     setbuf(stdout, NULL);
+    printf("\n[TESTLIB FOR VBSLACKER STARTING UP...]\n");
     printf("Making call to __initBasicLib()...\n"); 
     __initBasicLib();
+
     testOnEvents();
     testConversions();
 
         /* !!! This will be done automatically, eventually... */
+    printf("\n[TESTLIB FOR VBSLACKER SHUTTING DOWN...]\n");
     printf("Making call to __deinitBasicLib()...\n"); 
     __deinitBasicLib();
     return(0);
