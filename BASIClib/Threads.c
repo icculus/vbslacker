32e835d221e5b45a1f6e3ad8131cd112cf4ea61a
Ryan C. Gordon <icculus@icculus.org>
1998-07-28 02:43:03 +0000

    Not-quite-done.

--- BASIClib/Threads.c
+++ BASIClib/Threads.c
@@ -7,10 +7,19 @@
 /* !!! comment this thing. */
 
 #include <stdlib.h>
+#include <pthread.h>
 #include "Threads.h"
 
-static volatile int criticalSectionCount = 0;
-static volatile int *indexes;
+typedef struct
+{
+    void *(*fn)(void*);
+    void *args;
+} ThreadEntryArgs;
+
+typedef ThreadEntryArgs *PThreadEntryArgs;
+
+static volatile ThreadLock lock;
+static volatile pthread_t *indexes;
 static volatile int threadCount = 0;
 static volatile int maxIndex = 0;
 
@@ -18,15 +27,52 @@ static volatile int maxIndex = 0;
 void __initThread(int tidx); 
 void __deinitThread(int tidx); 
 
+
 void __initThreads(void)
 {
+    __createThreadLock(&lock);
     threadCount++;
     maxIndex++;
-    indexes = malloc(sizeof (int));
-    /* !!! indexes[0] = __OS__getTID(); */
+    indexes = malloc(sizeof (pthread_t));
+    indexes[0] = pthread_self();
 
     __initThread(0);
 } /* __initThreads */
+
+
+void __deinitThreads(void)
+{
+    __destroyThreadLock(&lock);
+} /* __deinitThreads */
+
+
+void __threadEntry(void *_args)
+{
+    PThreadStartArgs args = (PThreadEntryArgs) _args;
+
+    __initThread(args->tidx);
+    args->fn(args->args);
+    __memFree(args);
+    __terminateCurrentThread();
+} /* __threadStart */
+
+
+int __spinThread(void *(*_fn)(void *), void *_args)
+{
+    int i;
+    PThreadEntryArgs args = __memAlloc(sizeof (ThreadEntryArgs));
+    args->fn = _fn;
+    args->args = _args;
+
+    __obtainThreadLock(&lock);
+    for (i = 0; i < maxIndex; i++)
+    {
+        if (indexes[i] == NULL)
+            !!!
+    } /* for */
+
+    __releaseThreadLock(&lock);
+} /* __spinThread */
 
 
 int __getThreadCount(void)
@@ -43,27 +89,60 @@ int __getHighestThreadIndex(void)
 
 int __getCurrentThreadIndex(void)
 {
-    return(0);
+    pthread_t tid = pthread_self();
+    int retVal = -1;
+
+    __obtainThreadLock(&lock);
+
+    for (i = 0; retVal == -1; i++)
+    {
+        if (indexes[i] == tid)
+            retVal = i;
+    } /* for */
+
+    __releaseThreadLock(&lock);
+
+    return(retVal);
 } /* __getCurrentThreadIndex */
 
 
 void __threadTimeslice(void)
 {
+    /* !!! ?! */
 } /* __threadTimeslice */
 
 
-void __enterCriticalThreadSection(void)
+void __createThreadLock(PThreadLock pThreadLock)
 {
-    criticalSectionCount++;
-} /* __enterCriticalThreadSection */
+    int errType;
+
+    if (pthread_mutex_init(pThreadLock, NULL) != 0)
+    { 
+        errType = ((errno == ENOMEM) ? ERR_OUT_OF_MEMORY : ERR_INTERNAL_ERROR);
+        __runtimeError(errType);
+    } /* if */
+} /* __createThreadLock */
 
 
-void __exitCriticalThreadSection(void)
+void __destroyThreadLock(PThreadLock pThreadLock)
 {
-    criticalSectionCount--;
-    if (criticalSectionCount == 0)
-        ; /* do something. */
-} /* __exitCriticalThreadSection */
+    if (pthread_mutex_destroy(pThreadLock) != 0)
+        __runtimeError(ERR_INTERNAL_ERROR);
+} /* __destroyThreadLock */
+
+
+void __obtainThreadLock(PThreadLock pThreadLock)
+{
+    if (pthread_mutex_lock(pThreadLock) != 0)
+        __runtimeError(ERR_INTERNAL_ERROR);
+} /* __obtainThreadLock */
+
+
+void __releaseThreadLock(PThreadLock pThreadLock)
+{
+    if (pthread_mutex_unlock(pThreadLock) != 0)
+        __runtimeError(ERR_INTERNAL_ERROR);
+} /* __releaseThreadLock */
 
 
 /* end of Threads.c ... */
