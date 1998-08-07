a90f1dd38fcb5d1aed6f2a6a1f7aa6f6aa0f84e5
Ryan C. Gordon <icculus@icculus.org>
1998-08-07 18:41:45 +0000

    All (serious) bugs patched, and basically complete, except for mid$().

--- BASIClib/StringFunctions.c
+++ BASIClib/StringFunctions.c
@@ -7,6 +7,7 @@
  */
 
 #include <string.h>
+#include <ctype.h>
 #include "BasicString.h"
 #include "ErrorFunctions.h"
 
@@ -35,7 +36,7 @@ PBasicString right_DC_(PBasicString pStr, int count)
     else
     {
         retVal = __allocString(count, false);
-        memcpy(retVal, pStr->data + (pStr->length - count), count);
+        memcpy(retVal->data, pStr->data + (pStr->length - count), count);
     } /* else */
 
     return(retVal);
@@ -52,6 +53,19 @@ PBasicString left_DC_(PBasicString pStr, int count)
  *   returns : newly allocated string, per above details.
  */
 {
+    PBasicString retVal = NULL;
+
+    if (count < 0)
+        __runtimeError(ERR_ILLEGAL_FUNCTION_CALL);
+    else if (count > pStr->length)
+        __assignString(&retVal, pStr);
+    else
+    {
+        retVal = __allocString(count, false);
+        memcpy(retVal->data, pStr->data, count);
+    } /* else */
+
+    return(retVal);
 } /* left_DC_ */
 
 
@@ -67,16 +81,16 @@ PBasicString rtrim_DC_(PBasicString pStr)
     PBasicString retVal = NULL;
     int i;
 
-    for (i = pStr->length - 1; (i >= 0) && (retVal == NULL); i--)
-    {
-        if (pStr->data[i] != ' ')    /* End of trimmable space? */
-        {
-            retVal = __allocString(i, false);
-            memcpy(retVal->data, pStr->data, i);
-        } /* if */
-    } /* for */
+        /* loop to find end of trimmable chars... */
+    for (i = pStr->length - 1; (i >= 0) && (pStr->data[i] == ' '); i--);
 
-!!!
+    if (i >= 0)    /* chars to trim? */
+    {
+        retVal = __allocString(i + 1, false);
+        memcpy(retVal->data, pStr->data, i + 1);
+    } /* if */
+    else        /* no trim? Just copy it to retVal. */
+        __assignString(&retVal, pStr);
 
     return(retVal);
 } /* rtrim_DC_ */
@@ -91,6 +105,24 @@ PBasicString ltrim_DC_(PBasicString pStr)
  *   returns : newly allocated string with (pStr)'s left side trimmed.
  */
 {
+    PBasicString retVal = NULL;
+    int i;
+    int newSize;
+    int max = pStr->length;
+
+        /* loop to find end of trimmable chars... */
+    for (i = 0; (i <= max) && (pStr->data[i] == ' '); i++);
+
+    if (i < max)    /* chars to trim? */
+    {
+        newSize = max - i;
+        retVal = __allocString(newSize, false);
+        memcpy(retVal->data, pStr->data + i, newSize);
+    } /* if */
+    else        /* no trim? Just copy it to retVal. */
+        __assignString(&retVal, pStr);
+
+    return(retVal);
 } /* ltrim_DC_ */
 
 
@@ -102,7 +134,20 @@ PBasicString lcase_DC_(PBasicString pStr)
  *   returns : newly allocated string, per above details.
  */
 {
+    PBasicString retVal = NULL;
+    char *i;
+    char *max;
+
+    __assignString(&retVal, pStr);
+
+    max = (retVal->data + retVal->length) - 1;
+
+    for (i = retVal->data; i < max; i++)
+        *i = tolower(*i);
+
+    return(retVal);
 } /* lcase_DC_ */
+
 
 
 PBasicString ucase_DC_(PBasicString pStr)
@@ -113,6 +158,18 @@ PBasicString ucase_DC_(PBasicString pStr)
  *   returns : newly allocated string, per above details.
  */
 {
+    PBasicString retVal = NULL;
+    char *i;
+    char *max;
+
+    __assignString(&retVal, pStr);
+
+    max = (retVal->data + retVal->length) - 1;
+
+    for (i = retVal->data; i < max; i++)
+        *i = toupper(*i);
+
+    return(retVal);
 } /* ucase_DC_ */
 
 
@@ -129,6 +186,28 @@ int len(PBasicString pStr)
 {
     return(pStr->length);
 } /* len */
+
+
+PBasicString space_DC_(int length)
+/*
+ * Create a BasicString consisting of (length) (' ') chars.
+ *
+ *   params : length == number of spaces to put into string.
+ *  returns : newly allocated string.
+ */
+{
+    PBasicString retVal;
+
+    if (length < 0)
+        __runtimeError(ERR_ILLEGAL_FUNCTION_CALL);
+    else
+    {
+        retVal = __allocString(length, false);
+        memset(retVal->data, ' ', length);
+    } /* else */
+
+    return(retVal);
+} /* space_DC_ */
 
 /* end of StringFunctions.c ... */
 
