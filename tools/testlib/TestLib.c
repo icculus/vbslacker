fa14ed36745d7c42ff52cae75fb5fd7df630b7e0
Ryan C. Gordon <icculus@icculus.org>
1998-08-07 18:46:28 +0000

    Broke up mainline into a few drone functions, and added call to TestStringFunctions()...

--- tools/testlib/TestLib.c
+++ tools/testlib/TestLib.c
@@ -5,26 +5,41 @@
  */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "Initialize.h"
 #include "TestLib.h"
 
-int main(void)
+
+void initTestLib(void)
 {
     setbuf(stdout, NULL);
     printf("\n[TESTLIB FOR VBSLACKER STARTING UP...]\n");
     printf("Making call to __initBasicLib()...\n"); 
     __initBasicLib();
+} /* initTestLib */
 
-    testOnEvents();
-    testErrorFunctions();
-    testThreads();
-    testConversionFunctions();
-
+void deinitTestLib(void)
+{
     printf("\n[TESTLIB FOR VBSLACKER SHUTTING DOWN...]\n");
     printf("Making call to __deinitBasicLib()...\n"); 
 
         /* !!! This will be done automatically, eventually... */
     __deinitBasicLib();
+} /* deinitTestLib */
+
+
+int main(void)
+{
+    initTestLib();
+
+    testOnEvents();
+    testErrorFunctions();
+    testThreads();
+    testStringFunctions();
+    testConversionFunctions();
+
+    deinitTestLib();
     return(0);
 } /* main */
 
