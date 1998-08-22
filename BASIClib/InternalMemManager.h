8fbc270607f49a7ca6174c624fd5b01ac560e689
Ryan C. Gordon <icculus@icculus.org>
1998-08-22 08:52:36 +0000

    Added boxcar allocation prototypes.

--- BASIClib/InternalMemManager.h
+++ BASIClib/InternalMemManager.h
@@ -9,9 +9,23 @@
 
 #include <stdlib.h>
 
+void __initInternalMemManager(void);
+void __deinitInternalMemManager(void);
+
+    /* Runtime-checked Memory allocation... */
 void *__memAlloc(size_t byteCount);
 void *__memRealloc(void *oldPtr, size_t byteCount);
 void __memFree(void *ptr);
+
+    /* The "boxcar" system. */
+void __memAllocBoxcar(unsigned long boxcarId);
+void *__memAllocInBoxcar(unsigned long boxcarID, size_t byteCount);
+void *__memReallocInBoxcar(unsigned long boxcarId, void *ptr, size_t byteCount);
+void __memFreeInBoxcar(unsigned long boxcarId, void *ptr);
+void __memReleaseBoxcar(unsigned long boxcarId);
+void __memReleaseAllBoxcars(void);
+void __memReleaseBoxcarsBelow(unsigned long boxcarId);
+void __memReleaseBoxcarsAbove(unsigned long boxcarId);
 
 #endif
 
