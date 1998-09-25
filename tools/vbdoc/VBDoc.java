0a1e584aa74030f00293de46cf9f61daa0451cd4
Ryan C. Gordon <icculus@icculus.org>
1998-09-25 01:49:16 +0000

    First, non-compiled release. Not nearly done.

---
+++ tools/vbdoc/VBDoc.java
@@ -0,0 +1,18 @@
+/**
+ * This class reads specified VBDoc format files and converts them to a
+ *  given documentation format. This format depends on what implementation of
+ *  VBDocOutputable is used.
+ *
+ *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+import java.io.*;
+
+public class VBDoc extends Object
+{
+    public static void main(String args[])
+    {
+    } // main
+} // VBDoc
+
+// end of VBDoc.java ...
