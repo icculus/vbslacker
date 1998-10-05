e686a6ff4c9aa96987f1b2c4ba4dee7c14b1699e
Ryan C. Gordon <icculus@icculus.org>
1998-10-05 03:58:24 +0000

    initial release.

---
+++ BASIClib/PenFunctions.c
@@ -0,0 +1,34 @@
+/*
+ * Pen functions for BASIClib. Note that all of these are dummy functions,
+ *  the same Qbasic would return if there was no light pen attached to the
+ *  the system. Since Linux (and hell, Win95) have no specific light-pen
+ *  APIs, the best we could do is emulate a light pen with the mouse, but
+ *  that seems a little useless. These are just here for basic (er...VERY
+ *  basic) compatibility with older BASICs...modern BASICs don't include
+ *  this support at all.
+ *
+ *    Copyright (c) Ryan C. Gordon and Gregory S. Read.
+ */
+
+int pen(STATEPARAMS, int arg)
+{
+    switch (arg)
+    {
+        case 0: return(0);
+        case 1: return(0);
+        case 2: return(0);
+        case 3: return(0);
+        case 4: return(0);
+        case 5: return(0);
+        case 6: return(1);
+        case 7: return(1);
+        case 8: return(1);
+        case 9: return(1);
+        default: __runtimeError(STATEARGS, ERR_ILLEGAL_FUNCTION_CALL);
+    } /* switch */
+
+    return(0);  /* should never hit this, but just in case... */
+} /* pen */
+
+/* end of PenFunctions.c ... */
+
---
+++ BASIClib/PenFunctions.h
@@ -0,0 +1,17 @@
+/*
+ * Stub Pen functions for BASIClib, header.
+ *
+ *    Copyright (c) Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_PENFUNCTIONS_H_
+#define _INCLUDE_PENFUNCTIONS_H_
+
+#include "RegState.h"
+
+int pen(STATEPARAMS, int arg);
+
+#endif
+
+/* end of PenFunctions.h ... */
+
