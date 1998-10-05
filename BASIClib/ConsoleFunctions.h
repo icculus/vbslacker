da568eff57b2f938a1941647ad8faa85b7ee9d25
Ryan C. Gordon <icculus@icculus.org>
1998-10-05 03:32:26 +0000

    Renamed from Console.? ...

---
+++ BASIClib/ConsoleFunctions.h
@@ -0,0 +1,18 @@
+/*
+ * Header file for BASIClib console routines.
+ *
+ *   Copyright (c) 1998 Ryan Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_CONSOLE_H_
+#define _INCLUDE_CONSOLE_H_
+
+#include "RegState.h"
+
+void __initConsole(STATEPARAMS);
+void __deinitConsole(STATEPARAMS);
+
+#endif
+
+/* end of Console.h ... */
+
