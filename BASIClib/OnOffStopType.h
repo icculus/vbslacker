7f5f8f4029490f2080f2dc80aa03a21ce1c013fa
Ryan C. Gordon <icculus@icculus.org>
1998-10-09 08:32:32 +0000

    initial release.

---
+++ BASIClib/OnOffStopType.h
@@ -0,0 +1,16 @@
+/*
+ * !!! comment.
+ *
+ *
+ *   Copyright (c) Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_ONOFFSTOPTYPE_H_
+#define _INCLUDE_ONOFFSTOPTYPE_H_
+
+typedef enum { ON, OFF, STOP } OnOffStopType;
+
+#endif
+
+/* end of OnOffStopType.h ... */
+
