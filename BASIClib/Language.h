ef9dd1ec2ba5405678df0de4716e7625702d309a
Ryan C. Gordon <icculus@icculus.org>
1998-10-17 06:18:10 +0000

    initial release.

---
+++ BASIClib/Language.h
@@ -0,0 +1,27 @@
+/*
+ * Entry header for language-specific headers. Never directly #include
+ *  something like "English.h" or "German.h" ... it's better to
+ *  #include "Language.h" and -DLANG_ENGLISH on the command line.
+ *
+ *    Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_STDBASIC_H_
+
+#include "StdBasic.h"
+
+#else
+
+#ifndef _INCLUDE_LANGUAGE_H_
+#define _INCLUDE_LANGUAGE_H_
+
+#if defined LANG_ENGLISH
+    #include "English.h"
+#else
+    #error No language defined. Try "-DLANG_ENGLISH" ...
+#endif
+
+#endif
+
+/* end of Language.h ... */
+
