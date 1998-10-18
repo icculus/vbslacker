db59dfa686531bc8dabd0e26af1cc3b146c86d6a
Ryan C. Gordon <icculus@icculus.org>
1998-10-18 05:35:58 +0000

    Took out curses interface and made it MUCH more robust and abstract. Work is not completed yet, but here goes...

--- BASIClib/ConsoleFunctions.h
+++ BASIClib/ConsoleFunctions.h
@@ -9,14 +9,23 @@
 
 #include "StdBasic.h"
 
-void __initConsole(STATEPARAMS);
-void __deinitConsole(STATEPARAMS);
+void __initConsoleFunctions(STATEPARAMS);
+void __deinitConsoleFunctions(STATEPARAMS);
 void vbpV_print(STATEPARAMS, PVariant pVar);
-void vbpii_ViewPrint(STATEPARAMS, int topRow, int bottomRow);
-void vbp_ViewPrint(STATEPARAMS);
+void vbpii_viewPrint(STATEPARAMS, int topRow, int bottomRow);
+void vbp_viewPrint(STATEPARAMS);
 void vbp_cls(STATEPARAMS);
 int vbi_csrline(STATEPARAMS);
 int vbiA_pos(STATEPARAMS, void *pVar);
+
+extern void (*__deinitConsole)(STATEPARAMS);
+extern void (*__vbpS_print)(STATEPARAMS, PBasicString x);
+extern void (*__vbpii_viewPrint)(STATEPARAMS, int top, int bottom);
+extern void (*__vbp_viewPrint)(STATEPARAMS);
+extern void (*__vbp_cls)(STATEPARAMS);
+extern int  (*__vbi_csrline)(STATEPARAMS);
+extern int  (*__vbiA_pos)(STATEPARAMS, void *pVar);
+extern void (*__vbpiii_color)(STATEPARAMS, int fore, int back, int bord);
 
 #endif
 
