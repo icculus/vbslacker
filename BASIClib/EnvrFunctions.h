ef6a0d4101605603a8b37f2f1d55d623dd3ecb5b
Ryan C. Gordon <icculus@icculus.org>
1998-10-31 10:00:03 +0000

    Fixed some syntax problems...

--- BASIClib/EnvrFunctions.h
+++ BASIClib/EnvrFunctions.h
@@ -9,12 +9,14 @@
 
 #include "StdBasic.h"
 
+void __initEnvrFunctions(STATEPARAMS);
+void __deinitEnvrFunctions(STATEPARAMS);
 void vbpS_chdir(STATEPARAMS, PBasicString newDir);
 PBasicString vbSS_environ_DC_(STATEPARAMS, PBasicString envVarName);
 PBasicString vbSi_environ_DC_(STATEPARAMS, int n);
 void vbpS_environ(STATEPARAMS, PBasicString newEnvrStr);
-vbii_fre(STATEPARAMS, int arg);
-vbiS_fre(STATEPARAMS, PBasicString strExp);
+int vbii_fre(STATEPARAMS, int arg);
+int vbiS_fre(STATEPARAMS, PBasicString strExp);
 
 #endif
 
