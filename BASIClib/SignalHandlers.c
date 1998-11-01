1cd50c24926f4ee05973daf78dd81f0a378a6271
Ryan C. Gordon <icculus@icculus.org>
1998-11-01 10:56:45 +0000

    ...tiny updates. Not much.

--- BASIClib/SignalHandlers.c
+++ BASIClib/SignalHandlers.c
@@ -4,27 +4,45 @@
  *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
  */
 
-#include <stdio.h>  /* !!! remove this when signal handlers are written! */
 #include <signal.h>
+#include "SignalHandlers.h"
 
-#warning Get around to writing the signal handlers
-
-static void __sigintHandler(int sig)
-{
-    printf("sigint handler!\n");
-} /* __sigintHandler */
-
+#warning What to do about SIGWINCH?
 
 static void __crashHandler(int sig)
+/*
+ * This is called when the program is to be terminated due to a
+ *  SIGSEGV, SIGFPE, SIGBUS, etc...While poorly written BASIC
+ *  programs should throw runtime errors and exit gracefully,
+ *  they should never raise exceptions, so the kernel needs to
+ *  terminate them forcefully. Therefore, if such an event occurs,
+ *  we collect all the pertinent data we can, and try to send it
+ *  on to the BASIClib development team, so such problems (which
+ *  may be bugs in BASIClib, or poorly written code doing unexpected
+ *  things) can be eliminated.
+ *
+ */
 {
-    printf("crash handler!\n");
+    #warning write __crashHandler() !!!
+    exit(0);        /* ...and bail. */
 } /* __crashHandler */
 
 
+#ifdef WANTSIGWINCH
 static void __sigwinchHandler(int sig)
 {
     printf("sigwinch handler!\n");
 } /* __sigwinchHandler */
+#endif
+
+
+static void __sigintHandler(int sig)
+/*
+ * SIGINT is generated when CTRL-C is hit.
+ */
+{
+    exit(0);  /* This makes sure __deinitBasicLib() is called. */
+} /* __sigintHandler */
 
 
 void __initSignalHandlers(STATEPARAMS)
@@ -34,9 +52,11 @@ void __initSignalHandlers(STATEPARAMS)
     (void) signal(SIGILL, __crashHandler);
     (void) signal(SIGBUS, __crashHandler);
     (void) signal(SIGFPE, __crashHandler);
-    (void) signal(SIGWINCH, __sigwinchHandler);
-} /* __initSignalHandlers */
 
+#ifdef WANTSIGWINCH
+    (void) signal(SIGWINCH, __sigwinchHandler);
+#endif
+} /* __initSignalHandlers */
 
 /* end of SignalHandlers.c ... */
 
