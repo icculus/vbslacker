fa4cfd497ea4dc7472fa6abcae62f6f332b19e88
Ryan C. Gordon <icculus@icculus.org>
1998-12-08 04:26:59 +0000

    Lots of changes, lots more handy, now...

--- BASIClib/Platform.h
+++ BASIClib/Platform.h
@@ -1,7 +1,31 @@
 /*
- * Some lowlevel stuff that all vbSlacker code should include.
- *
+ * Some platform-specific stuff that all vbSlacker code should include.
  *  This file is automagically included by StdBasic.h and BasicLib.h ...
+ *
+ * You need to define one (and only one) of the following before including
+ *  this file (if you don't, the compile generates a fatal error...) :
+ *
+ *    LINUXELF_I386  for intel-based ELF-binary linux machines.
+ *    WIN32_I386     for intel-based Win32 machines (Win95/98/NT).
+ *    (...and perhaps more to come...)
+ *
+ * And you'll end up with these defines when this file is done processing...
+ *
+ *    FUNCNAME_PREPEND ... equals "_", or "", or whatever, depending on
+ *                         what the platform's compiler adds to the beginning
+ *                         of function identifiers. You need to know this for
+ *                         inline assembly calls.
+ *
+ *    ARCHITECTURE     ... equals "i386" (...and perhaps more to come...),
+ *                         depending on what the platform's processor type
+ *                         is.
+ *
+ *    STACK_DIRECTION  ... equals (-1) if the stack on a given platform grows
+ *                         downward, or (1) if stack grows upward.
+ *
+ *    EOL_STRING       ... equals "\n" or "\r\n" (...and perhaps more to
+ *                         come...) depending on what characters signify
+ *                         an end-of-line on a given platform.
  *
  *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
  */
@@ -21,21 +45,22 @@
 
 /* Verify that a supported platform has been defined... */
 
-#if defined LINUX_I386
-#    define ARCHITECTURE_I386
+#if defined LINUXELF_I386
+#    define ARCHITECTURE      "i386"
+#    define FUNCNAME_PREPEND  ""
+#    define STACK_DIRECTION   -1
+#    define EOL_STRING        "\n"
 #elif defined WIN32_I386
-#    define ARCHITECTURE_I386
+#    define ARCHITECTURE      "i386"
+#    define FUNCNAME_PREPEND  "_"
+#    define STACK_DIRECTION   -1
+#    define EOL_STRING        "\r\n"
 #else
 #    error No valid platform has been defined.
 #    error Please use one of the following with the commandline/Makefile:
-#    error  -DLINUX_I386
+#    error  -DLINUXELF_I386
 #    error  -DWIN32_I386
 #endif /* defined platform */
-
-#ifdef ARCHITECTURE_I386
-#    define STACK_DIRECTION -1
-#endif
-
 
 #endif /* _INCLUDE_PLATFORM_H_ */
 #endif /* _INCLUDE_STDBASIC_H_ */
