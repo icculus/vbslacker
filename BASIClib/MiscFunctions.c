5338d5df187e860e3951b931a1fe712b1652ff8e
Ryan C. Gordon <icculus@icculus.org>
1998-12-20 09:24:22 +0000

    Initial krapola.

---
+++ BASIClib/MiscFunctions.c
@@ -0,0 +1,37 @@
+/*
+ * Everything that didn't fit somewhere else...
+ *
+ *  Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#include "MiscFunctions.h"
+
+void vbp_clear(STATEPARAMS)
+/*
+ * There's no way in hell we're supporting this. Microsoft docs
+ *  says that if it's used anywhere but the mainline, it throws
+ *  an ILLEGAL FUNCTION CALL, so we'll just throw that anyhow.
+ *  Later VBs don't even HAVE this call. Sheesh.
+ *
+ *     params : void.
+ *    returns : Always throws error.
+ */
+{
+    __runtimeError(STATEARGS, ERR_ILLEGAL_FUNCTION_CALL);
+} /* vbp_clear */
+
+
+void vbpNNl_clear(STATEPARAMS, __long stackSize);
+/*
+ * Refer to vbp_clear()...
+ *
+ *     params : void.
+ *    returns : Always throws error.
+ */
+{
+    __runtimeError(STATEARGS, ERR_ILLEGAL_FUNCTION_CALL);
+} /* vbNNl_clear */
+
+/* end of MiscFunctions.c ... */
+
+
