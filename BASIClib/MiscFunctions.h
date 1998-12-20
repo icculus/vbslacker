78a5ca2af98fa54f24ad289ded92394483108332
Ryan C. Gordon <icculus@icculus.org>
1998-12-20 09:24:22 +0000

    Initial krapola.

---
+++ BASIClib/MiscFunctions.h
@@ -0,0 +1,19 @@
+/*
+ * Everything that didn't fit somewhere else...the HEADER.
+ *
+ *  Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_MISCFUNCTIONS_H_
+#define _INCLUDE_MISCFUNCTIONS_H_
+
+#include "StdBasic.h"
+
+void vbp_clear(STATEPARAMS);
+void vbpNNl_clear(STATEPARAMS, __long stackSize);
+
+#endif /* _INCLUDE_MISCFUNCTIONS_H_ */
+
+/* end of MiscFunctions.h ... */
+
+
