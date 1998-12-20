59677ba1b0f435d7aabea14e1439242a7ee887c8
Ryan C. Gordon <icculus@icculus.org>
1998-12-20 21:48:20 +0000

    Initial release. Stub files...

---
+++ BASIClib/RandomFunctions.c
@@ -0,0 +1,14 @@
+/*
+ * Random number functions.
+ *
+ *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#include "RandomFunctions.c"
+
+/* !!! still need: */
+/* rnd */
+/* randomize */
+
+/* end of RandomFunctions.c ... */
+
