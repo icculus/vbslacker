8061e2021bccf2a9ff423331de44959a28c9a5df
Ryan C. Gordon <icculus@icculus.org>
1998-12-20 21:48:20 +0000

    Initial release. Stub files...

---
+++ BASIClib/RandomFunctions.h
@@ -0,0 +1,15 @@
+/*
+ * BASIC support library: random number functions header.
+ *
+ *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_RANDOMFUNCTIONS_H_
+#define _INCLUDE_RANDOMFUNCTIONS_H_
+
+#include "StdBasic.h"
+
+#endif
+
+/* end of RandomFunctions.h ... */
+
