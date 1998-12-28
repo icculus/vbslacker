eae4900f6ed788e23f0762fe8cb22b8c055aa296
Ryan C. Gordon <icculus@icculus.org>
1998-12-28 09:53:17 +0000

    Removed some no-longer relevant INITFLAG_* constants...

--- BASIClib/Initialize.h
+++ BASIClib/Initialize.h
@@ -15,13 +15,11 @@
 
 #define INITFLAG_NO_FLAGS              0
 #define INITFLAG_NOT_INITIALIZED       0
-#define INITFLAG_DISABLE_RESUME        1
-#define INITFLAG_DISABLE_ONEVENTS      2
-#define INITFLAG_DISABLE_RANGE_CHECKS  4
-#define INITFLAG_DISABLE_NUM_OVERFLOW  8
-#define INITFLAG_DISABLE_VARIANTS      16
-#define INITFLAG_DISABLE_CONSOLE       32
-#define INITFLAG_UNIX_FILE_SYSTEM      64
+#define INITFLAG_DISABLE_RANGE_CHECKS  1
+#define INITFLAG_DISABLE_NUM_OVERFLOW  2
+#define INITFLAG_DISABLE_VARIANTS      4
+#define INITFLAG_DISABLE_CONSOLE       8
+#define INITFLAG_UNIX_FILE_SYSTEM      16
 
     /* function prototypes ... */
 void __initBasicLib(STATEPARAMS, long flags, int argc, char **argv);
