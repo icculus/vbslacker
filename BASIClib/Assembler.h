195da658df8dcb9bdaf00cc3feacd6c627fa9f08
Ryan C. Gordon <icculus@icculus.org>
1998-12-31 09:10:37 +0000

    - Copyright was updated. - This file is now an entry point, and the actual inline assembly has    been moved to elsewhere for specific platforms, like Asm_I386_GNU.h ...

--- BASIClib/Assembler.h
+++ BASIClib/Assembler.h
@@ -1,85 +1,43 @@
 /*
  * Some lowlevel stuff that all vbSlacker code should include.
  *
- * ALL ASSEMBLY LANGUAGE STATEMENTS SHOULD BE ISOLATED TO THIS FILE,
- *  UNDER THE CORRECT ARCHITECTURE. This is for abstraction purposes.
- *
- *  This file is automagically included by StdBasic.h and BasicLib.h ...
- *
- *   Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ *   Copyright (c) 1999 Ryan C. Gordon and Gregory S. Read.
  */
 
 #ifndef _INCLUDE_STDBASIC_H_
-
-#include "StdBasic.h"
-
+#   include "StdBasic.h"
 #else
 
-#ifndef _INCLUDE_ASSEMBLER_H_
-#define _INCLUDE_ASSEMBLER_H_
+#   ifndef _INCLUDE_ASSEMBLER_H_
+#       define _INCLUDE_ASSEMBLER_H_
 
-    /*
-     * God, inline asm is SCARY lookin' in gcc, isn't it?
-     */
-#if (defined __GNUC__)
+#       define _INCLUDING_LOCAL_ASM_CODE_
 
-    #if (defined I386)
+#       if (defined __GNUC__)
+#           if (defined I386)
+#               include "Asm_I386_GNU.h"
+#           else
+#               define ___NOASM___
+#           endif
 
-            /*
-             *  Get the stack pointer, and store it in *retVal.
-             */
-        #define __getStackPointer(retVal) __asm__ __volatile__              \
-                                                  (                         \
-                                                    "movl %%esp, %0\n\t"    \
-                                                        : "=q" (*retVal)    \
-                                                  )
+#       elif (defined __MSVC__)
+#           if (defined I386)
+#               include "Asm_I386_MSVC.h"
+#           else
+#               define ___NOASM___
+#           endif
 
-            /*
-             *  Get the base pointer, and store it in *retVal.
-             */
-        #define __getBasePointer(retVal) __asm__ __volatile__               \
-                                                  (                         \
-                                                    "movl %%ebp, %0\n\t"    \
-                                                        : "=q" (*retVal)    \
-                                                  )
+#       else
+#           define ___NOASM___
+#       endif
 
+#       undef _INCLUDING_LOCAL_ASM_CODE_
 
-            /*
-             * Unconditional jump to a specified address, for
-             *  better or for worse.
-             */
-        #define __jump(addr) __asm__ __volatile__ (                         \
-                                                    "jmpl *%0\n\t"          \
-                                                        :                   \
-                                                        : "q" (addr)        \
-                                                  )
+#       if (defined ___NOASM___)
+#           error No ASM written for this platform/compiler, yet...
+#       endif /* defined ___NOASM___ */
 
-            /*
-             * This is used by __triggerOnError()...adjust the
-             *  stack, and unconditionally jump to an address...
-             */
-        #define __setStackAndJump(base, stack, addr) __asm__ __volatile__   \
-                                                  (                         \
-                                                    "movl %0, %%ebp \n\t"   \
-                                                    "movl %1, %%esp \n\t"   \
-                                                    "jmpl *%2       \n\t"   \
-                                                        :                   \
-                                                        : "q" (base),       \
-                                                          "q" (stack),      \
-                                                          "q" (addr)        \
-                                                  )
-
-
-    #else
-        #error No ASM written for this platform, yet...
-    #endif /* defined (architecture) */
-
-#else
-    #error No idea how to use inline assembly on your compiler.
-#endif /* defined (compiler system) */
-
-
-#endif /* defined _INCLUDE_ASSEMBLER_H_ */
+#   endif /* defined _INCLUDE_ASSEMBLER_H_ */
 #endif /* defined _INCLUDE_STDBASIC_H_ */
 
 /* end of Assembler.h ... */
