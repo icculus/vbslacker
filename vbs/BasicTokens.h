8725ceb2886496d273774abe53a6683bc0beff10
Gregory S. Read <zeph@clutteredmind.org>
1999-01-18 02:40:12 +0000

    Uhhh..these probably don't do much yet.

---
+++ vbs/BasicTokens.h
@@ -0,0 +1,19 @@
+/*
+ * Declarations for the BasicTokens class.
+ *
+ *  Copyright (c) 1998 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#ifndef _INCLUDE_BASICTOKENS_H_
+#define _INCLUDE_BASICTOKENS_H_
+
+class BasicTokens
+{
+    public:
+        BasicTokens();
+        ~BasicTokens();
+    private:
+};
+
+#endif
+/* end of BasicTokens.h */
