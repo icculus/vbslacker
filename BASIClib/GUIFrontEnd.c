a4aa230c705c01143c4e5f87676101162da18bca
Ryan C. Gordon <icculus@icculus.org>
1999-05-12 03:51:40 +0000

    initial release. Doesn't do anything but take up space in the repository.

---
+++ BASIClib/GUIFrontEnd.c
@@ -0,0 +1,45 @@
+/*
+ * This code is responsible for creating the structure for vbSlacker
+ *  GUI support.
+ *
+ *   Copyright (c) 1999 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#include "GUIFrontEnd.h"
+
+static __boolean initializedGUIFrontEnd = __false;
+
+
+void __initGUIFrontEnd(void)
+/*
+ * Initialize the GUI framework, if the program wants to enable it.
+ *
+ *
+ */
+{
+    if (__getInitFlags() & INITFLAG_ENABLE_GUIFRONTEND)
+    {
+        initializedGUIFrontEnd = __true;
+    } /* if */
+} /* __initGUIFrontEnd */
+
+
+void __deinitGUIFrontEnd(void)
+/*
+ * Deinitialize the GUI framework, if the program initialized it in the
+ *  first place.
+ *
+ *    params : void.
+ *   returns : void.
+ */
+{
+    if (initializedGUIFrontEnd)
+    {
+        /* !!! deinitialize it. */
+        initializedGUIFrontEnd = __false;
+    } /* if */
+} /* __deinitGUIFrontEnd */
+
+
+/* end of GUIFrontEnd.c ... */
+
---
+++ BASIClib/GUIFrontEnd.h
@@ -0,0 +1,18 @@
+/*
+ * Header file for vbSlacker's GUI front end framework.
+ *
+ *   Copyright (c) 1999 Ryan C. Gordon and Gregory S. Read.
+ */
+
+#include <StdBasic.h>
+
+/*
+ * These are implemented within BASIClib...
+ */
+
+void __initGUIFrontEnd(void);
+void __deinitGUIFrontEnd(void);
+
+
+/* end of GUIFrontEnd.h ... */
+
