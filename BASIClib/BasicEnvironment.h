63481ca1777ae9785fa0047f155d60498b4335f0
Ryan C. Gordon <icculus@icculus.org>
1999-05-15 21:41:01 +0000

    initial release. Honest.

---
+++ BASIClib/BasicEnvironment.h
@@ -0,0 +1,35 @@
+/*
+ * BASIC environment properties...Environment variable names, file and
+ *  directory names can go in here...
+ *
+ *   Copyright (c) 1999 Ryan C. Gordon and Gregory S. Read.
+ *    Written by Ryan C. Gordon.
+ */
+
+#ifndef _INCLUDE_STDBASIC_H_
+
+#include "StdBasic.h"
+
+#else
+
+#ifndef _INCLUDE_BASICENVIRONMENT_H_
+#define _INCLUDE_BASICENVIRONMENT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define __ENVSTR_HOME        "HOME"
+#define __ENVSTR_VBHOME      "VBHOME"
+#define __DIRNAME_VBHOME     ".vbslacker"
+#define __FILENAME_REGISTRY  "registry"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INCLUDE_BASICSTRING_H_ */
+#endif /* _INCLUDE_STDBASIC_H_ */
+
+/* end of BasicString.h ... */
+
