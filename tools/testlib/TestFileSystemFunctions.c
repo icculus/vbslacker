18df4ed6873f7208a26673574f2820b520a9e3e2
Ryan C. Gordon <icculus@icculus.org>
1999-06-05 02:27:41 +0000

    Added test for NAME and KILL...

--- tools/testlib/TestFileSystemFunctions.c
+++ tools/testlib/TestFileSystemFunctions.c
@@ -5,7 +5,85 @@
  */
 
 #include <stdio.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include "BasicLib.h"
+
+
+void test_kill(void)
+/*
+ * Test KILL() functionality.
+ *
+ *    params : void.
+ *   returns : void.
+ */
+{
+    __ONERRORVARS;
+    PBasicString fileName = __createString("testName", false);
+    struct stat statBuf;
+
+
+    __ONERRORINIT;
+    __setOnErrorHandler(nameChoked);
+
+    printf("Testing Kill...");
+
+    _vbpS_kill(fileName);
+
+    if (stat(__basicStringToAsciz(fileName), &statBuf) == 0)
+        printf("\n - File was not killed.\n");
+    else
+    {
+        if (errno == ENOENT)
+            printf("done.\n");
+        else
+            printf("\n - File is now unstat()able, but for wrong reason.\n");
+    } /* else */
+
+    __exitCleanupOnError;
+    return;
+
+__insertLineLabel(nameChoked);
+
+    printf("\n - Threw error. (#%ld)\n", _vbl_err());
+    __exitCleanupOnError;
+} /* test_kill */
+
+
+void test_name(void)
+/*
+ * Test NAME() functionality.
+ *
+ *    params : void.
+ *   returns : void.
+ */
+{
+    __ONERRORVARS;
+    PBasicString copyThis = __createString("testFileCopy", false);
+    PBasicString copyHere = __createString("testName", false);
+    struct stat statBuf;
+
+
+    __ONERRORINIT;
+    __setOnErrorHandler(nameChoked);
+
+    printf("Testing Name...");
+
+    _vbpSS_name(copyThis, copyHere);
+
+    if (stat(__basicStringToAsciz(copyHere), &statBuf) != 0)
+        printf("\n - renaming is unstat()able.\n");
+    else
+        printf("done.\n");
+
+    __exitCleanupOnError;
+    return;
+
+__insertLineLabel(nameChoked);
+
+    printf("\n - Threw error. (#%ld)\n", _vbl_err());
+    __exitCleanupOnError;
+} /* test_name */
 
 
 void test_filecopy(void)
@@ -18,8 +96,9 @@ void test_filecopy(void)
 {
     __ONERRORVARS;
     PBasicString copyHere = __createString("testFileCopy", false);
+    struct stat statBuf;
 
-#ifdef LINUX
+#if (defined LINUX)
     PBasicString fileName = __createString("/etc/inittab", false);
 #elif (defined WIN32)
     PBasicString fileName = __createString("C:\\msdos.sys", false);
@@ -35,14 +114,17 @@ void test_filecopy(void)
 
     _vbpSS_filecopy(fileName, copyHere);
 
+    if (stat(__basicStringToAsciz(copyHere), &statBuf) != 0)
+        printf("\n - new copy is unstat()able.\n");
+    else
+        printf("done.\n");
 
-    printf("done.\n");
     __exitCleanupOnError;
     return;
 
 __insertLineLabel(fileCopyChoked);
 
-    printf(" - Threw error. (#%ld)\n", _vbl_err());
+    printf("\n - Threw error. (#%ld)\n", _vbl_err());
     __exitCleanupOnError;
 } /* test_filecopy */
 
@@ -58,6 +140,8 @@ void testFileSystemFunctions(void)
     printf("\n[TESTING FILESYSTEM FUNCTIONS...]\n");
 
     test_filecopy();
+    test_name();
+    test_kill();
 } /* testFileSystemFunctions */
 
 
