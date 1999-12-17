7fac4bf2dd90a1c369fd7abda337dca9e1a45642
Gregory S. Read <18175637+readgs@users.noreply.github.com>
1999-12-17 04:01:24 +0000

    Added comments, cleaned up code, and added basic exception handling.

--- parser/PostParser.h
+++ parser/PostParser.h
@@ -1,30 +1,30 @@
-/* Header file for PostParser class
+/* PostParser - Header file for PostParser class
+ *
  * Copyright (c) 1999 Ryan C. Gordon and Gregory S. Read.
  * 
- * Description
- *    The ANTLR grammer will pass along parsed VB statements to this class
- *    for furthur processing.
+ * The ANTLR grammer will pass along parsed VB statements to this class for
+ * furthur processing.
  */
-/*
-12345678901234567890123456789012345678901234567890123456789012345678901234567890
-*/
 
 #ifndef __POSTPARSER_H
 #define __POSTPARSER_H
 
 #include "antlr/Token.hpp"
+#include "antlr/ParserException.hpp"
 
 typedef enum BasicScope {SCOPE_PUBLIC, SCOPE_PRIVATE, SCOPE_DIM, SCOPE_FRIEND};
 typedef enum Boolean {FALSE = 0, TRUE = 1};
 
 class PostParser
 {
-   public:
-      PostParser();
-      ~PostParser();
+public:
+    PostParser();
+    ~PostParser();
 
-      void VarDecl(BasicScope Scope, RefToken VarName, RefToken DataType,
-         Boolean WithEvents, Boolean AsNew);
+	void ReportError(ParserException *pException);
+	
+    void VarDecl(BasicScope Scope, RefToken VarName, RefToken DataType,
+                 Boolean WithEvents, Boolean AsNew);
 };
 
 #endif
--- parser/main.cpp
+++ parser/main.cpp
@@ -1,18 +1,43 @@
 /* This will exist until we turn this parse into a shared library */
 
-#include <iostream>
+//#include <iostream>
+#include <strstream.h>
+#include <stdio.h>
 #include "BasicLexer.hpp"
 #include "BasicParser.hpp"
+#include "PostParser.h"
+
+// Maximum length of a single VB line
+#define MAX_LINE_LENGTH     1024
 
 int main()
 {
-	try {
-		BasicLexer lexer(std::cin);
-		BasicParser parser(lexer);
+	char strInput[MAX_LINE_LENGTH];
+	istrstream *pInputStream;
+	BasicLexer *pLexer;
+	BasicParser *pParser;
+	PostParser MyPostParser;
 
-	   parser.start();
-	} catch(exception& e) {
-	   std::cerr << "exception: " << e.what() << std::endl;
+	/* Until I find a better way to do this, I'm creating a new lexer and
+	 * parser for each line of VB code.  Right now, I can't get the parser
+	 * to parse multiple lines without getting all weird if there are
+	 * syntax errors.  So I'm doing it this way until I think of a better
+	 * option.  This better be temporary. :-)
+	 */
+	while(fgets(strInput, MAX_LINE_LENGTH, stdin) != NULL)
+	{
+		// Create new objects for parsing
+		pInputStream = new istrstream(strInput);
+		pLexer = new BasicLexer(*pInputStream);
+		pParser = new BasicParser(*pLexer);
+
+		// Start parsing the buffer
+		pParser->start(&MyPostParser);
+
+		// Delete the objects since we're done with them.
+		delete pInputStream;
+		delete pLexer;
+		delete pParser;
 	}
 
 	return 0;
