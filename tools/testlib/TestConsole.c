c171f792ecfb8ba087eef62f0f36d8fd715e4232
Ryan C. Gordon <icculus@icculus.org>
1999-12-23 08:18:03 +0000

    Added more error handling, so tests continue if, say, CursesConsole throws  an error for unsupported COLOR calls, etc...

--- tools/testlib/TestConsole.c
+++ tools/testlib/TestConsole.c
@@ -51,14 +51,34 @@ void testPrinting(void)
 
 void testClearing(void)
 {
+    __ONERRORVARS;
+    __ONERRORINIT();
+    __setOnErrorHandler(_cls_test_error_handler_);
+    __setInstructs(_start_cls_test_, _end_cls_test_);
+
+__insertLineLabel(_start_cls_test_);
     _vbp_cls();
-    __printAsciz("Screen should have cleared via CLS. Three second delay...");
+    __printAsciz("Screen should have cleared via CLS. ");
+__insertLineLabel(_end_cls_test_);
+    __printAsciz("Three second delay...");
     sleep(3);
+    __exitCleanupOnError();
+    return;
+
+__insertLineLabel(_cls_test_error_handler_);
+    __printNewLine();
+    __printAsciz("Runtime Error: CLS not supported?");
+    __printNewLine();
+    __printNewLine();
+    __resumeNext();
 } /* testClearing */
 
 
 void testLocate(void)
 {
+    __ONERRORVARS;
+    __ONERRORINIT();
+
     _vbp_cls();
     __printAsciz("123456789012345678901234567890");
     __printNewLine();
@@ -88,6 +108,11 @@ void testLocate(void)
     __printAsciz("  - The 'Y' should be at (x=5, y=8)...");
     __printNewLine();
     __printAsciz("  - The 'Z' should be at (x=9, y=8)...");
+
+
+    __setOnErrorHandler(_locate_test_error_handler_);
+    __setInstructs(_start_locate_test_, _end_locate_test_);
+__insertLineLabel(_start_locate_test_);
     _vbpii_locate(5, 4);
     __printAsciz("X");
     _vbpiN_locate(8);
@@ -95,9 +120,21 @@ void testLocate(void)
     _vbpNi_locate(9);
     __printAsciz("Z");
     _vbpii_locate(17, 1);
+
+__insertLineLabel(_end_locate_test_);
     __printAsciz("  ...delaying 10 seconds...");
     __printNewLine();
     sleep(10);
+
+    __exitCleanupOnError();
+    return;
+
+__insertLineLabel(_locate_test_error_handler_);
+    __printNewLine();
+    __printAsciz("Runtime Error: LOCATE not supported?");
+    __printNewLine();
+    __printNewLine();
+    __resumeNext();
 } /* testLocate */
 
 
@@ -133,11 +170,15 @@ void testViewPrint()
     __ONERRORVARS;
     __integer i;
 
-    __ONERRORINIT;
+    __ONERRORINIT();
     _vbp_cls();
     for (i = 0; i < 300; i++)
         __printAsciz("abcdefghijklmnopqrstuvwxyz");
 
+
+    __setOnErrorHandler(_viewprint_test_error_handler_);
+    __setInstructs(_start_viewprint_test_, _end_viewprint_test_);
+__insertLineLabel(_start_viewprint_test_);
     _vbpii_viewPrint(10, 15);
     _vbp_cls();
     __printAsciz("view print: Lines 10 - 15 should be blank."
@@ -147,21 +188,39 @@ void testViewPrint()
     testScrolling();
     _vbp_viewPrint();
     _vbp_cls();
+
+__insertLineLabel(_end_viewprint_test_);
     __printAsciz("Whole console should be clear, now...except this text.");
     sleep(3);
+    __exitCleanupOnError();
+    return;
+
+__insertLineLabel(_viewprint_test_error_handler_);
+    __printNewLine();
+    __printAsciz("Runtime Error: VIEW PRINT not supported?");
+    __printNewLine();
+    __printNewLine();
+    __resumeNext();
 } /* testViewPrint */
 
 
 void testColor(void)
 {
+    __ONERRORVARS;
     int i;
     char buffer[10];
+
+    __ONERRORINIT();
 
     _vbp_cls();
     __printAsciz("About to test COLOR command...");
     __printNewLine();
     __printNewLine();
 
+    __setOnErrorHandler(_color_test_error_handler_);
+    __setInstructs(_start_color_test_, _end_color_test_);
+
+__insertLineLabel(_start_color_test_);
     for (i = 0; i <= 15; i++)
     {
         sprintf(buffer, " %d ", i);
@@ -172,10 +231,93 @@ void testColor(void)
     _vbpi_color(7);  /* default color? */
     __printNewLine();
     __printNewLine();
-    __printAsciz("Color testing complete. Waiting 5 seconds...");
+
+    __printAsciz("Color testing complete. ");
+__insertLineLabel(_end_color_test_);
+
+    __printAsciz("Waiting 5 seconds...");
     __printNewLine();
     sleep(5);
+    __exitCleanupOnError();
+    return;
+
+__insertLineLabel(_color_test_error_handler_);
+    __printNewLine();
+    __printAsciz("Runtime Error: COLOR not supported?");
+    __printNewLine();
+    __printNewLine();
+    __resumeNext();
 } /* testColor */
+
+
+void testBeep(void)
+{
+    __ONERRORVARS;
+    __ONERRORINIT();
+
+    _vbp_cls();
+
+    __setOnErrorHandler(_beep_test_error_handler_);
+    __setInstructs(_start_beep_test_, _end_beep_test_);
+
+__insertLineLabel(_start_beep_test_);
+    __printAsciz("...here comes a beep in 3 seconds...");
+    sleep(3);
+    _vbp_beep();
+    __printAsciz("There it was!");
+    __printNewLine();
+
+__insertLineLabel(_end_beep_test_);
+    __printAsciz("BEEP testing complete. Three second delay...");
+    __printNewLine();
+    sleep(3);
+    __exitCleanupOnError();
+    return;
+
+__insertLineLabel(_beep_test_error_handler_);
+    __printNewLine();
+    __printAsciz("Runtime Error: BEEP not supported?");
+    __printNewLine();
+    __printNewLine();
+    __resumeNext();
+} /* testBeep */
+
+void testSound(void)
+{
+    int i;
+    __ONERRORVARS;
+    __ONERRORINIT();
+
+    _vbp_cls();
+
+    __setOnErrorHandler(_sound_test_error_handler_);
+    __setInstructs(_start_sound_test_, _end_sound_test_);
+
+__insertLineLabel(_start_sound_test_);
+    __printAsciz("...here comes a raising sound in 3 seconds...");
+    sleep(3);
+
+        /* This is almost right from QBASIC's example code... */
+    for (i = 440; i <= 1000; i += 5)
+        _vbpif_sound(i, ((__single) i) / 1000.0);
+
+    __printAsciz("There it was!");
+    __printNewLine();
+
+__insertLineLabel(_end_sound_test_);
+    __printAsciz("SOUND testing complete. Three second delay...");
+    __printNewLine();
+    sleep(3);
+    __exitCleanupOnError();
+    return;
+
+__insertLineLabel(_sound_test_error_handler_);
+    __printNewLine();
+    __printAsciz("Runtime Error: SOUND not supported?");
+    __printNewLine();
+    __printNewLine();
+    __resumeNext();
+} /* testSound */
 
 
 void cleanup(void)
@@ -189,12 +331,14 @@ void cleanup(void)
 
 void testConsole(void)
 {
-    testPrinting();
-    testClearing();
-    testLocate();
-    testScrolling();
-    testViewPrint();
-    testColor();
+//    testPrinting();
+//    testClearing();
+//    testLocate();
+//    testScrolling();
+//    testViewPrint();
+//    testColor();
+    testBeep();
+//    testSound();
     cleanup();
 } /* testConsole */
 
