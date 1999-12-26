4758c0b0c63f2a26f70e2e60f513c169d9eac642
Gregory S. Read <zeph@clutteredmind.org>
1999-12-26 00:02:02 +0000

    Indents are 4 spaces, not 3.

--- parser/main.cpp
+++ parser/main.cpp
@@ -12,33 +12,33 @@
 
 int main()
 {
-   char strInput[MAX_LINE_LENGTH];
-   istrstream *pInputStream;
-   BasicLexer *pLexer;
-   BasicParser *pParser;
-   PostParser MyPostParser;
+    char strInput[MAX_LINE_LENGTH];
+    istrstream *pInputStream;
+    BasicLexer *pLexer;
+    BasicParser *pParser;
+    PostParser MyPostParser;
+ 
+    /* Until I find a better way to do this, I'm creating a new lexer and
+     * parser for each line of VB code.  Right now, I can't get the parser
+     * to parse multiple lines without getting all weird if there are
+     * syntax errors.  So I'm doing it this way until I think of a better
+     * option.  This better be temporary. :-)
+     */
+    while(fgets(strInput, MAX_LINE_LENGTH, stdin) != NULL)
+    {
+        // Create new objects for parsing
+        pInputStream = new istrstream(strInput);
+        pLexer = new BasicLexer(*pInputStream);
+        pParser = new BasicParser(*pLexer);
 
-   /* Until I find a better way to do this, I'm creating a new lexer and
-    * parser for each line of VB code.  Right now, I can't get the parser
-    * to parse multiple lines without getting all weird if there are
-    * syntax errors.  So I'm doing it this way until I think of a better
-    * option.  This better be temporary. :-)
-    */
-   while(fgets(strInput, MAX_LINE_LENGTH, stdin) != NULL)
-   {
-      // Create new objects for parsing
-      pInputStream = new istrstream(strInput);
-      pLexer = new BasicLexer(*pInputStream);
-      pParser = new BasicParser(*pLexer);
+        // Start parsing the buffer
+        pParser->start(&MyPostParser);
 
-      // Start parsing the buffer
-      pParser->start(&MyPostParser);
+        // Delete the objects since we're done with them.
+        delete pInputStream;
+        delete pLexer;
+        delete pParser;
+    }
 
-      // Delete the objects since we're done with them.
-      delete pInputStream;
-      delete pLexer;
-      delete pParser;
-   }
-
-   return 0;
+    return 0;
 }
